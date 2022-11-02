a4fbf4bba3d7b2fb394581b166026ee7af0498de
rajad <rajad@LAPTOP-DLDHI5RC>
2022-11-02 12:14:14 +0530

    Added comment

--- CodeFirst/Startup.cs
+++ CodeFirst/Startup.cs
@@ -16,6 +16,7 @@ namespace CodeFirst
             Configuration = configuration;
         }
 
+        //Added comment
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
